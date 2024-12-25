19a22c8d71d494f70ea541806e2cb3db80964b0e
dasu malam <dasumalam4@gmail.com>
2024-12-25 21:07:17 +0530

    add taylor taylor

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -4,9 +4,9 @@ let sidebar = document.getElementById("sidebar");
 sidebar.addEventListener("click", () => {
   if (nav.classList.contains("hidden")) {
     nav.classList.remove("hidden");
-    nav.classList.add("flex", "flex-col", "top-16", "absolute", "gap-2", "z-30", "right-[25%]", "bg-[#af8f579f]","py-3","px-5");
+    nav.classList.add("flex", "flex-col", "top-16", "absolute", "gap-2", "z-30", "right-[3%]", "bg-[#af8f579f]", "py-3","px-5");
   } else {
     nav.classList.add("hidden");
-    nav.classList.remove("flex", "flex-col", "top-16", "absolute", "gap-2", "z-30", "right-[25%]", "bg-[#af8f579f]", "py-3", "px-5");
+    nav.classList.remove("flex", "flex-col", "top-16", "absolute", "gap-2", "z-30", "right-[3%]", "bg-[#af8f579f]", "py-3", "px-5");
   }
 });
